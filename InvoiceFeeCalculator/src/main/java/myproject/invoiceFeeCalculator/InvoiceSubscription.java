b3c7a4ead78e673987e4f398ae262e95a4efdea3
gbenash175 <gbenash175@yahoo.com>
2015-02-07 20:55:56 -0500

    first commit

---
+++ InvoiceFeeCalculator/src/main/java/myproject/invoiceFeeCalculator/InvoiceSubscription.java
@@ -0,0 +1,5 @@
+package myproject.invoiceFeeCalculator;
+
+public class InvoiceSubscription {
+
+}
---
+++ InvoiceFeeCalculator/src/test/java/myproject/invoiceFeeCalculator/TestInvoiceSubscription.java
@@ -0,0 +1,15 @@
+package myproject.invoiceFeeCalculator;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestInvoiceSubscription extends TestCase {
+
+	
+	@Test
+	public void testInvoiceBelowHundred(){
+		
+		
+	}
+}
