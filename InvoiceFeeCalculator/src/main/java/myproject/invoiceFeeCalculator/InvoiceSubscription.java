3d143df4d85dd50df4b921268d5fd5005271cb28
gbenash175 <gbenash175@yahoo.com>
2015-02-09 10:18:17 -0500

    case 1: basic fee calculation

--- InvoiceFeeCalculator/src/main/java/myproject/invoiceFeeCalculator/InvoiceSubscription.java
+++ InvoiceFeeCalculator/src/main/java/myproject/invoiceFeeCalculator/InvoiceSubscription.java
@@ -1,5 +1,16 @@
 package myproject.invoiceFeeCalculator;
 
 public class InvoiceSubscription {
+	
+	
+	protected double calc20PercentFee(double invoiceMoney ){
+		return 20 * invoiceMoney / 100;
+	}
+	
+	protected double calc10PercentFee(double invoiceMoney){
+		return 10 * invoiceMoney / 100;
+	}
 
 }
+
+  
--- InvoiceFeeCalculator/src/test/java/myproject/invoiceFeeCalculator/TestInvoiceSubscription.java
+++ InvoiceFeeCalculator/src/test/java/myproject/invoiceFeeCalculator/TestInvoiceSubscription.java
@@ -1,15 +1,42 @@
 package myproject.invoiceFeeCalculator;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import junit.framework.TestCase;
 
+
 public class TestInvoiceSubscription extends TestCase {
 
+	InvoiceSubscription calc = null;
+	
+	@Before
+	public void setup(){
+		
+	}
+	
 	
 	@Test
 	public void testInvoiceBelowHundred(){
+		calc = new InvoiceSubscription();
+		double invoiceMoney1 = 900.00;
+		double invoiceMoney2 = 99000.00;		
+		double invoiceFee = calc.calc20PercentFee(invoiceMoney1);
+		double invoiceFee2 = calc.calc20PercentFee(invoiceMoney2);
 		
+		assertEquals(180.0, invoiceFee);
+		assertEquals(19800.0, invoiceFee2);		
+	}
+	
+	@Test
+	public void testInvoiceAboveHundred(){
+		calc = new InvoiceSubscription();
+		double invoiceMoney1 = 200000.00;
+		double invoiceMoney2 = 990000.00;		
+		double invoiceFee = calc.calc10PercentFee(invoiceMoney1);
+		double invoiceFee2 = calc.calc10PercentFee(invoiceMoney2);
 		
+		assertEquals(20000.0, invoiceFee);
+		assertEquals(99000.0, invoiceFee2);		
 	}
 }
